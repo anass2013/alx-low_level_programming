18f482a026cea683d74c40636c57265e3e3f93e1
anass2013 <mr.anasmadad@gmail.com>
2023-07-16 06:56:41 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,22 +8,22 @@
 
 int main(void)
 {
-	int i = 0;
-	int j = 0;
-	int k = 0;
+	int i = '0';
+	int j = '0';
+	int k = '0';
 
-	while (i <= 7)
+	while (i <= '7')
 	{
-		while (j <= 8)
+		while (j <= '8')
 		{
-			while (k <= 9)
+			while (k <= '9')
 			{
 				if (i < j && j < k)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (i + j + k != 24)
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i + j + k != '24')
 					{
 						putchar(',');
 						putchar(' ');
