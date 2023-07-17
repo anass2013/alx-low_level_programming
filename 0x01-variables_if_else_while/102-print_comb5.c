d04991770ab8ea521d2de800b4d198d7463ff5ce
anass2013 <mr.anasmadad@gmail.com>
2023-07-17 06:05:39 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Toujours 0 (Success)
+ */
+
+int main(void)
+{
+	int nb1, nb2;
+
+	for (nb1 = 0; nb1 <= 99; nb1++)
+	{
+		for (nb2 = 0; nb2 <= 99; nb2++)
+		{
+			if (nb1 != nb2)
+			{
+				putchar((nb1 / 10) + '0');
+				putchar((nb2 % 10) + '0');
+				putchar(' ');
+				putchar((nb2 / 10) + '0');
+				putchar((nb2 % 10) + '0');
+				if (!(nb1 == 98 && nb2 == 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
