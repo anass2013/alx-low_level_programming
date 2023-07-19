743967caf8408582445896a9248a84051e6201d7
anass2013 <mr.anasmadad@gmail.com>
2023-07-19 04:35:04 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar((p / 100) + '0');
 					_putchar((p / 10) % 10 + '0');
 				}
-				else if (p >= 10 && p <= 99)
+				else if (p <= 99 && p >= 10)
 					_putchar((p / 10) + '0');
 				_putchar((p % 10) + '0');
 			}
