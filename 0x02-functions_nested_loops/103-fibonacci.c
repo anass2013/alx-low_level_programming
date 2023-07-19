95dcbe38fe56d28889e98dad228d00cbf1764747
anass2013 <mr.anasmadad@gmail.com>
2023-07-19 14:56:10 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,19 +8,20 @@
 
 int main(void)
 {
-	unsigned long Fb1 = 0, Fb2 = 2, Fb3;
-	long S = Fb1 + Fb2;
+	unsigned long Fb1 = 0, Fb2 = 1, Fb3;
+	long S;
 
 	while (Fb2 <= 4000000)
 	{
-		Fb3 = 4 * Fb2 + Fb1;
+		Fb3 = Fb1 + Fb2;
 		if (Fb3 > 4000000)
 			break;
 
-		S = S + Fb3;
+		if ((Fb3 % 2) == 0)
+			S += Fb3;
+
 		Fb1 = Fb2;
 		Fb2 = Fb3;
-		S += Fb2;
 	}
 
 	printf("%ld\n", S);
