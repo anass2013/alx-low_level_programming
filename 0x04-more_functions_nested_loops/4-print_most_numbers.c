759a837543c44a01d566b80a0eed96e96d6d5451
anass2013 <mr.anasmadad@gmail.com>
2023-07-20 08:21:37 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	while (i >= 0 && i <= 9)
+	for (i = 0; i < 10; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;
 		_putchar(i + '0');
-		i++;
 	}
+
 	_putchar('\n');
 }
