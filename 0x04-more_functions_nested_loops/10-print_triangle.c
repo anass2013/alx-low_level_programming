a266a17117f69fe3a564280101404dd0e61f7533
anass2013 <mr.anasmadad@gmail.com>
2023-07-20 19:07:10 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,9 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
+		_putchar('\n');
+	else
 	{
 		for (i = 1, i <= size; i++)
 		{
@@ -25,6 +27,4 @@ void print_triangle(int size)
 			_putchar('\n');
 		}
 	}
-
-	_putchar('\n');
 }
