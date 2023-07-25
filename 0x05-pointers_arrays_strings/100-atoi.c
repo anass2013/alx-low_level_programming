a6be1d70d25ac1903dfc9a1c035fdcc9ac465f1b
anass2013 <mr.anasmadad@gmail.com>
2023-07-25 06:10:03 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,15 +13,14 @@ int _atoi(char *s)
 	unsigned int i = 0;
 	int sg = 1;
 
-	while (*s++)
-	{
+	do {
 		if (*s == '-')
 			sg *= -1;
 		else if (*s >= '0' && *s <= '9')
 			i = (i * 10) + (*s - '0');
 		else if (i > 0)
 			break;
-	}
+	} while (*s++);
 
 	return (i * sg);
 }
