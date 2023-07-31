ffa929f54a8280d64643f7402248e86b386ae141
anass2013 <mr.anasmadad@gmail.com>
2023-07-31 15:57:03 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,11 +10,11 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *p1 = haystack;
-	char *p2 = needle;
-
 	for (; *haystack != '\0'; haystack++)
 	{
+		char *p1 = haystack;
+		char *p2 = needle;
+
 		while (*p1 == *p2 && *p2 != '\0')
 		{
 			p1++;
