9bd66ad223922956f4f87552163d4e3741439cda
anass2013 <mr.anasmadad@gmail.com>
2023-10-08 05:53:33 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,28 @@
 #include "main.h"
 
 /**
- * *_setfill - fills memory with a constant type
- * @s: pointer
- * @b: constant
- * @n: maximum bytes to use
- * Return: string
- */
-
-char *_setfill(char *s, char b, unsigned int n)
-{
-	char *p = s;
-
-	while (n--)
-		*s++ = b;
-
-	return (p);
-}
-
-/**
- * *_calloc - allocates memory for an array, using malloc
- * @nmemb: array length
- * @size: size of each element
- * Return: pointer
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: byte size
+ *
+ * Return: void pointer to array space
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *c;
+	char *m;
 
-	if (size == 0 || nmemb == 0)
-		return (NULL);
-	c = malloc(sizeof(int) * nmemb);
-
-	if (c == 0)
+	if (!nmemb || !size)
 		return (NULL);
 
-	_setfill(c, 0, sizeof(int) * nmemb);
+	m = malloc(nmemb * size);
 
-	return (c);
+	if (!m)
+		return (NULL);
+
+	nmemb *= size;
+
+	while (nmemb--)
+		m[nmemb] = 0;
+	return (m);
 }
